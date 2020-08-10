9bdb2d472f99ce52e0d27327cc6acc0e8843caad
pavel ilin <refugeebase@gmail.com>
2020-08-10 12:03:12 -0400

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Fragment } from 'react';
+import { decode, encode } from 'base-64'
 import WorldMap from './Components/WorldMap'
+
+
+if (!global.btoa) { global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
 const App = () => {
 
