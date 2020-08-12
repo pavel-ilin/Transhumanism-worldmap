317ad8349658250946e21fcf0556d26ede2acf09
pavel ilin <refugeebase@gmail.com>
2020-08-12 12:34:19 -0400

    fetch data from firebase storage

--- src/Components/WorldMap.js
+++ src/Components/WorldMap.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState, Fragment } from 'react';
 import { event, select, geoPath, geoMercator, min, max, scaleLinear, zoom } from "d3";
 import Modal from 'react-modal';
 import '../App.css';
-import CombinedGeoData from "../utils/CombinedGeoData.json";
+// import OldCombinedGeoData from "../utils/CombinedGeoData.json";
 import firebase from '../utils/firebaseConfig';
 import PopUp from './PopUp'
 
@@ -21,20 +21,17 @@ const WorldMap = () => {
         const svgRef = useRef()
         const [clickData, setClickData] = useState(null)
         const [isOpen, setIsOpen] = useState(false);
+        const [CombinedGeoData, setData] = useState(false)
         
-        const storage = firebase.storage()
-        const geodata = storage.ref().child("CombinedGeoData.json").getDownloadURL()
-        .then(function(url) {
-          var xhr = new XMLHttpRequest();
-          xhr.responseType = 'blob';
-          xhr.onload = function(event) {
-            var blob = xhr.response;
-          };
-          xhr.open('GET', url);
-          xhr.send();
-          console.log(url)
-        })
-
+        const getGeoData = () => {
+          const storage = firebase.storage()
+          storage.ref().child("/CombinedGeoData.json").getDownloadURL()
+          .then(function(url) {
+            fetch(url)
+            .then(r => r.json())
+            .then(resp => {setData(resp)})
+          })  
+        }
 
         let div = select("body")
           .append("div")   
@@ -81,54 +78,54 @@ const WorldMap = () => {
           setIsOpen(false);
         }
         
-        useEffect(() => {
-          const svg = select(svgRef.current)
-    
-          const minProp = min(CombinedGeoData.features, feature => feature.properties.present);
-          const maxProp = max(CombinedGeoData.features, feature => feature.properties.present);
-    
-          const colorScale = scaleLinear()
-            .domain([minProp, maxProp])
-            .range(["#ccc", "orange"]);
-    
-          const width = window.innerWidth
-          const height = window.innerHeight
 
-          const projection = geoMercator()
-            .fitSize([width, height], CombinedGeoData)
-            .precision(100);
-    
-          const pathGenerator = geoPath().projection(projection)
-    
-          svg.selectAll('.country')
-            .data(CombinedGeoData.features)
-            .join('path')
-            .attr('class', 'country')
-            .attr("fill", feature => colorScale(feature.properties.present))
-            .attr('d', feature => pathGenerator(feature))
-            .on('mouseover', feature => { mouseOver(feature) })
-            .on('mouseout', feature => { mouseOut(feature) })
-            .on("click", feature => { mapClick(feature) })
-            .call(zoom()
-                  .on('zoom', () => {
-                    svg.attr("transform", event.transform)
-                 }))
-        }, [])
+        useEffect(() => {
+          if (!CombinedGeoData) {
+            getGeoData()
+          }
+          else {
+            const svg = select(svgRef.current)
+            const minProp = min(CombinedGeoData.features, feature => feature.properties.present);
+            const maxProp = max(CombinedGeoData.features, feature => feature.properties.present);
+            const colorScale = scaleLinear()
+                .domain([minProp, maxProp])
+                .range(["#ccc", "orange"]);
+            const width = window.innerWidth
+            const height = window.innerHeight
+            const projection = geoMercator()
+                .fitSize([width, height], CombinedGeoData)
+                .precision(100);
+            const pathGenerator = geoPath().projection(projection)
+            svg.selectAll('.country')
+                .data(CombinedGeoData.features)
+                .join('path')
+                .attr('class', 'country')
+                .attr("fill", feature => colorScale(feature.properties.present))
+                .attr('d', feature => pathGenerator(feature))
+                .on('mouseover', feature => { mouseOver(feature) })
+                .on('mouseout', feature => { mouseOut(feature) })
+                .on("click", feature => { mapClick(feature) })
+                .call(zoom()
+                    .on('zoom', () => {
+                      svg.attr("transform", event.transform)
+                  }))
+          }
+        }, [CombinedGeoData, getGeoData])
 
           return (
                 <Fragment>
-                  <Modal
-                    isOpen={isOpen}
-                    onRequestClose={closeModal}
-                    style={modalStyle}
-                    ariaHideApp={false}
-                  >
-                    <PopUp data={clickData}/>
-                  </Modal>
-                  <div className={'App'}>Transhumanism</div>
-                  <div className={'App'}> 
-                    <svg className={'map'} ref={svgRef}/>
-                  </div>
+                      <Modal
+                        isOpen={isOpen}
+                        onRequestClose={closeModal}
+                        style={modalStyle}
+                        ariaHideApp={false}
+                      >
+                        <PopUp data={clickData}/>
+                      </Modal>
+                      <div className={'App'}>Transhumanism</div>
+                      <div className={'App'}> 
+                        <svg className={'map'} ref={svgRef}/>
+                      </div>
                 </Fragment>
       )
     }
